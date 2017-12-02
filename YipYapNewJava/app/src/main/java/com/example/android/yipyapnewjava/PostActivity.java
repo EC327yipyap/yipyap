e614396bf93b792c51599287123dc9e24225774d
Erin Thomas <eethomas@bu.edu>
2017-12-02 10:55:39 -0500

    made PostActivity.java class!

---
+++ YipYapNewJava/app/src/main/java/com/example/android/yipyapnewjava/PostActivity.java
@@ -0,0 +1,12 @@
+package com.example.android.yipyapnewjava;
+
+import android.app.Activity;
+
+import com.example.android.yipyapnewjava;
+
+/**
+ * Created by erin.thomas on 12/2/17.
+ */
+
+public class PostActivity extends Activity implements yipyapnewjava {
+}
