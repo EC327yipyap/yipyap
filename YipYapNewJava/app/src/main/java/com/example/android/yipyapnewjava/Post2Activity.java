59b630e1968c009690e52b05103b6b43fff62489
laurajoyerb <laurajoy@bu.edu>
2017-12-03 09:21:01 -0500

    added pics, connected activities, deleted random yipyapp folder

--- YipYapNewJava/app/src/main/java/com/example/android/yipyapnewjava/Post2Activity.java
+++ YipYapNewJava/app/src/main/java/com/example/android/yipyapnewjava/Post2Activity.java
@@ -1,7 +1,9 @@
 package com.example.android.yipyapnewjava;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Post2Activity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class Post2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post2);
+    }
+
+    public void launchFeed (View view)
+    {
+        Intent i = new Intent(this, FeedActivity.class);
+        startActivity(i);
     }
 }
