d408aa62c62a6cb464bb8d1172942c4297c7d413
MohUddin <muddin1@bu.edu>
2017-12-03 03:12:41 -0500

    fb backend stuff
    -added dependencies (11.6.2 is the most recent update)
    -added permission to connect to internet
    -used firebase class to set some stuff up

--- YipYapNewJava/app/src/main/java/com/example/android/yipyapnewjava/MainActivity.java
+++ YipYapNewJava/app/src/main/java/com/example/android/yipyapnewjava/MainActivity.java
@@ -5,13 +5,21 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
+import com.firebase.client.Firebase;
+
 
 public final class MainActivity extends AppCompatActivity {
+
+    Firebase myFirebase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.setContentView(R.layout.activity_main);
+
+        Firebase.setAndroidContext(this);
+
+        myFirebase = new Firebase("https://yipyap-44cfc.firebaseio.com");
     }
 
 
