a3eaca53358cdc2cd1b05f4b112f95f0fa576c2c
laurajoyerb <laurajoy@bu.edu>
2017-12-03 21:51:34 -0500

    added imagebuttons and info for each image in feed

--- YipYapNewJava/app/src/main/java/com/example/android/yipyapnewjava/FeedActivity.java
+++ YipYapNewJava/app/src/main/java/com/example/android/yipyapnewjava/FeedActivity.java
@@ -5,13 +5,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+
 public class FeedActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feed);
+
     }
+
 
     public void launchMain (View view)
     {
