31819c5904c84b320a49c59406d437f86823a8c3
MohUddin <muddin1@bu.edu>
2017-12-04 09:40:07 -0500

    posting to database! done!
    lets gooo (we need to test it tho xD)

--- YipYapNewJava/app/src/main/java/com/example/android/yipyapnewjava/Malone.java
+++ YipYapNewJava/app/src/main/java/com/example/android/yipyapnewjava/Malone.java
@@ -1,24 +1,48 @@
 package com.example.android.yipyapnewjava;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 public class Malone extends AppCompatActivity {
 
     private ImageButton mSelectImage;
+    private EditText    mPostTitle;
+    private EditText    mPostDescription;
+    private Button      mSubmitButton;
+    private Uri         mImageUri = null;
 
     private static final int GALLERY_REQUEST = 1;
+
+    private StorageReference mStorage;
+    private ProgressDialog mProgress;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_malone);
 
+        mStorage = FirebaseStorage.getInstance().getReference();
+
         mSelectImage = (ImageButton) findViewById(R.id.imageSelect);
+        mPostTitle = (EditText) findViewById(R.id.postTitle);
+        mPostDescription = (EditText) findViewById(R.id.postDescription);
+        mSubmitButton = (Button) findViewById(R.id.submitButton);
+
+        mProgress = new ProgressDialog(this);
+
 
         mSelectImage.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -29,6 +53,38 @@ public class Malone extends AppCompatActivity {
                 startActivityForResult(galleryIntent, GALLERY_REQUEST);
             }
         });
+
+        mSubmitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                startPosting();
+                
+            }
+        });
+    }
+
+    private void startPosting() {
+
+        mProgress.setMessage("Posting puppies soon...");
+        mProgress.show();
+        String title_val = mPostTitle.getText().toString().trim();
+        String desc_val = mPostDescription.getText().toString().trim();
+
+        if(!TextUtils.isEmpty(title_val) && !TextUtils.isEmpty(desc_val) && mImageUri != null){
+
+            StorageReference filePath = mStorage.child("Post_Images").child(mImageUri.getLastPathSegment());
+
+            filePath.putFile(mImageUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+
+                    Uri downloadUrl = taskSnapshot.getDownloadUrl();
+                    mProgress.dismiss();
+                }
+            });
+        }
+
     }
 
     @Override
@@ -37,9 +93,9 @@ public class Malone extends AppCompatActivity {
 
         if(requestCode == GALLERY_REQUEST && resultCode == RESULT_OK){
 
-            Uri imageUri = data.getData();
+            mImageUri = data.getData();
 
-            mSelectImage.setImageURI(imageUri);
+            mSelectImage.setImageURI(mImageUri);
         }
     }
 }
