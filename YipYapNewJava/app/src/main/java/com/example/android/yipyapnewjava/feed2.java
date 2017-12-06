aab8ffb4fb46c1d4698e9eab2ba8e938937c3fa0
laurajoyerb <laurajoy@bu.edu>
2017-12-05 21:12:57 -0500

    more useless commented stuff

--- YipYapNewJava/app/src/main/java/com/example/android/yipyapnewjava/feed2.java
+++ YipYapNewJava/app/src/main/java/com/example/android/yipyapnewjava/feed2.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -12,8 +13,12 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class feed2 extends AppCompatActivity {
 
@@ -33,37 +38,85 @@ public class feed2 extends AppCompatActivity {
         mFeedList = (RecyclerView) findViewById(R.id.feed_list);
         mFeedList.setHasFixedSize(true);
         mFeedList.setLayoutManager(new LinearLayoutManager(this));
+
+//        // Attach a listener to read the data at our posts reference
+//        mDatabase.addValueEventListener(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(DataSnapshot dataSnapshot) {
+//                Malone post = dataSnapshot.getValue(Malone.class);
+//                System.out.println(post);
+//            }
+//
+//            @Override
+//            public void onCancelled(DatabaseError databaseError) {
+//                System.out.println("The read failed: " + databaseError.getCode());
+//            }
+//        });
     }
+
 
     @Override
     protected void onStart() {
         super.onStart();
 
-        FirebaseRecyclerAdapter<PostMalone, PostViewHolder> firebaseRecyclerAdapter = new FirebaseRecyclerAdapter<PostMalone, PostViewHolder>(
-                PostMalone.class,
-                R.layout.feed_row,
-                PostViewHolder.class,
-                mDatabase
-        ) {
+        // Mo's attempts
+//        FirebaseRecyclerAdapter<PostMalone, PostViewHolder> firebaseRecyclerAdapter = new FirebaseRecyclerAdapter<PostMalone, PostViewHolder>(
+//                PostMalone.class,
+//                R.layout.feed_row,
+//                PostViewHolder.class,
+//                mDatabase
+//        ) {
+//
+//            @Override
+//            public PostViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+//                return null;
+//            }
+//
+//            @Override
+//            protected void onBindViewHolder(PostViewHolder holder, int position, PostMalone model) {
+//
+//            }
+//
+//
+//            protected void populateViewHolder(PostViewHolder viewHolder, PostMalone model, int position){
+//
+//                viewHolder.setTitle(model.getTitle());
+//                viewHolder.setDescription(model.getDescription());
+//            }
+//        };
+//        mFeedList.setAdapter(firebaseRecyclerAdapter);
 
-            @Override
-            public PostViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-                return null;
-            }
 
-            @Override
-            protected void onBindViewHolder(PostViewHolder holder, int position, PostMalone model) {
-
-            }
-
-
-            protected void populateViewHolder(PostViewHolder viewHolder, PostMalone model, int position){
-
-                viewHolder.setTitle(model.getTitle());
-                viewHolder.setDescription(model.getDescription());
-            }
-          };
-        mFeedList.setAdapter(firebaseRecyclerAdapter);
+        // Laura Joy's attempts
+//        FirebaseRecyclerOptions<PostMalone> options =
+//                new FirebaseRecyclerOptions.Builder<PostMalone>()
+//                        .setQuery(mDatabase, PostMalone.class)
+//                        .build();
+//
+//        FirebaseRecyclerAdapter firebaseRecyclerAdapter = new FirebaseRecyclerAdapter<PostMalone, PostViewHolder>(options)
+//        {
+//            @Override
+//            public PostViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+//                View v = LayoutInflater.from(parent.getContext())
+//                        .inflate(R.layout.feed_row, parent, false);
+//
+//                PostViewHolder vh = new PostViewHolder(v);
+//                return vh;
+//
+//            }
+//
+//            @Override
+//            protected void onBindViewHolder(PostViewHolder holder, int position, PostMalone model) {
+//                // We have to put something here!
+//            }
+//
+//            protected void populateViewHolder(PostViewHolder viewHolder, PostMalone model, int position){
+//
+//                viewHolder.setTitle(model.getTitle());
+//                viewHolder.setDescription(model.getDescription());
+//            }
+//        };
+//        mFeedList.setAdapter(firebaseRecyclerAdapter);
     }
 
     public static class PostViewHolder extends RecyclerView.ViewHolder {
